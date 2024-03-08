799eccc6a2d150023d9c6614779bd1f5b3e50702
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-03-08 23:41:11 +0900

    solution by using serial queue

---
+++ BankAccount-Solution1wSerialQueue.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class BankAccount {
+    var balance: Double
+    
+    init(balance: Double) {
+        self.balance = balance
+    }
+    
+    func withdraw(_ amount: Double) {
+        if balance >= amount {
+            let processingTime = UInt32.random(in: 0...3)
+            print("[Withdraw] Processing for $\(amount), \(processingTime) seconds")
+            sleep(processingTime)
+            balance -= amount
+            print("Balance is $\(balance)")
+        }
+    }
+}
+
+let bankAccount = BankAccount(balance: 500)
+
+let queue = DispatchQueue(label: "SerialQueue")
+
+queue.async {
+    bankAccount.withdraw(300)
+}
+
+queue.async {
+    bankAccount.withdraw(500)
+}
+
+
